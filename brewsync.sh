0b9687e56bd52deed9aeb896f07cd6cd4d210614
Sebastien Lambla <seb@serialseb.com>
2019-05-30 10:54:22 +0200

    Brew synchronization

---
+++ brewsync.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ev
+
+! ln -s Brewfile ~/.Brewfile
+#which hub || brew install hub
+
+brew bundle install --global
+
+brew bundle dump --global --force
+
+git add Brewfile && git commit -m "Brew package sync"
+ 
